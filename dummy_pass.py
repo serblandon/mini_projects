842fe951c8ff6784fec60f7a36f4246bfcb904d1
Serban <serban.crasovan@student.upt.ro>
2022-09-19 09:23:48 +0300

    without dictionary

--- dummy_pass.py
+++ dummy_pass.py
@@ -4,14 +4,19 @@ import random
 
 def function():
 
-    collection = string.digits + string.ascii_letters + string.punctuation
+    charac_type = {
+        "1": string.ascii_lowercase,
+        "2": string.ascii_uppercase,
+        "3": string.digits,
+        "4": string.punctuation
+    }
 
-    nb_of_length_choices = random.randrange(2, 5)
+    nb_of_length_choices = random.randrange(2,5)
 
     array = []
     j = 0
     while j < nb_of_length_choices:
-        array.append(random.randrange(5, 20))
+        array.append(random.randrange(5,20))
         j += 1
 
     length = int(secrets.choice(array))
@@ -23,12 +28,13 @@ def function():
     i = 0
     while i < length:
         nb = secrets.choice(dict_index)
-        password += str(secrets.choice(collection))
+        password += str(secrets.choice(charac_type.get(nb)))
         i += 1
 
     print(length)
+
     return password
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(function())
