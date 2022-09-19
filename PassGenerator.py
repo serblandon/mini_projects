4e08ebbda786dae50733b33592b609c4a4aa2c5b
Serban <serban.crasovan@student.upt.ro>
2022-09-19 09:30:54 +0300

    No random

--- PassGenerator.py
+++ PassGenerator.py
@@ -1,17 +1,18 @@
 import secrets
 import string
-import random
 
 def function():
 
     collection = string.digits + string.ascii_letters + string.punctuation
 
-    nb_of_length_choices = random.randrange(2, 5)
+    nb_of_length_choices = [2, 3, 4, 5]
+    nb_of_lentgh_final = secrets.choice(nb_of_length_choices)
 
+    ranges = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     array = []
     j = 0
-    while j < nb_of_length_choices:
-        array.append(random.randrange(5, 20))
+    while j < nb_of_lentgh_final:
+        array.append(secrets.choice(ranges))
         j += 1
 
     length = int(secrets.choice(array))
